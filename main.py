f96a70f5f834062392c702e50adc9a2e0dba4c4c
makinenimpradeep <pradeep.makineni10@gmail.com>
2023-03-18 10:58:20 +0530

    updated treasure & main py files

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import conditionalStatements.nested_rollercoaster
 import conditionalStatements.pizza_orderexcersise
 import conditionalStatements.logical_operators
+import conditionalStatements.treasure_island
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
+    # Press ⌘F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
@@ -12,59 +14,5 @@ if __name__ == '__main__':
     conditionalStatements.pizza_orderexcersise.pizza_order()
     conditionalStatements.nested_rollercoaster.roller_coaster()
     conditionalStatements.logical_operators.logical_operator()
-
-# Logical oprators
-# name1 = input("what is your name")
-# name2 = input("what is there name")
-#
-# name = name1 + name2
-# lower_case = name.lower()
-#
-# t = lower_case.count('t')
-# r = lower_case.count('r')
-# u = lower_case.count('u')
-# e = lower_case.count('e')
-#
-# true = t + r + u + e
-#
-# l = lower_case.count('l')
-# o = lower_case.count('o')
-# v = lower_case.count('v')
-# e = lower_case.count('e')
-#
-# love = l + o + v + e
-#
-# calculator = str(true) + str(love)
-# print(calculator)
-# love_score = int(calculator)
-# if (love_score >= 60) and (love_score <= 100):
-#     print(f"your love score {love_score}, {name1} and {name2} are like coke and mentos")
-# elif (love_score >= 30) or (love_score <= 59):
-#     print(f"your love score {love_score}, you like each other")
-# else:
-#     print(f"your love score {love_score}, are okay with each other")
-
-
-# Excercise
-# print("welcome to Treasure island ")
-# navigation = input("what way would u like to travel left or right? ")
-# if navigation == 'left':
-#     print("wait for the next step")
-#
-#     decision = input("do you want to swim or wait? ")
-#     if decision == 'wait':
-#         door_color = input("which color door do you want to exit Red, Blue or Yellow? ")
-#         if door_color == 'yellow':
-#             print("you are the winner")
-#         else:
-#             print("your game over")
-#     else:
-#         print("your game over")
-# else:
-#     print("your game over")
-
-# randomisation & List
-# import random
-# random_integer = random.randint(1, 10)
-# print(random_integer)
+    conditionalStatements.treasure_island.treasure_island()
 
